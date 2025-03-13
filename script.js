84468927ef4f091205a8eb027684eb519f6d0d70
fajarsabana <fajar.sabana@gmail.com>
2025-03-13 13:54:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Initialize the Leaflet Map
     const map = L.map('map', {
-        scrollWheelZoom: false, // Disable scroll zoom to prevent interference with page scroll
+        scrollWheelZoom: true, // Disable scroll zoom to prevent interference with page scroll
         dragging: true,         // Enable dragging everywhere
     }).setView([-6.2088, 106.8456], 10); // Center the map on Jakarta
 
